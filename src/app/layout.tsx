73bf527fddfef184e43315117fed5ceb03e79d76
Murtuza Vohra <murtuza52@gmail.com>
2025-09-23 15:17:56 +0000

    Try fixing this error: `Build Error: Module not found: Can't resolve '@v

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import './globals.css';
 import { Toaster } from '@/components/ui/toaster';
 import 'katex/dist/katex.min.css';
+import { SpeedInsights } from '@vercel/speed-insights/next';
 
 export const metadata: Metadata = {
   title: 'Leo Creator',
@@ -23,6 +24,7 @@ export default function RootLayout({
       </head>
       <body className="font-body antialiased">
         {children}
+        <SpeedInsights />
         <Toaster />
       </body>
     </html>
