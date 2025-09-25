07690396e3bced5bbcc5b8c244774cb5f6f12e86
Murtuza Vohra <murtuza52@gmail.com>
2025-09-25 06:33:00 +0000

    Try fixing this error: `Runtime Error: Hydration failed because the serv

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -21,11 +21,8 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet" />
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4404974079606262" crossOrigin="anonymous"></script>
-        <script async custom-element="amp-auto-ads" src="https://cdn.ampproject.org/v0/amp-auto-ads-0.1.js"></script>
       </head>
       <body className="font-body antialiased">
-        <amp-auto-ads type="adsense" data-ad-client="ca-pub-4404974079606262"></amp-auto-ads>
         {children}
         <SpeedInsights />
         <Toaster />
