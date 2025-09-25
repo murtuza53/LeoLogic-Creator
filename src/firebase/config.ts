b1e974585fecb665c9483a2dad4347bf466f29b1
Murtuza Vohra <murtuza52@gmail.com>
2025-09-25 07:34:21 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-4692583593-18d7f",
+  "appId": "1:879807515009:web:4111b2591b8c44ad094080",
+  "apiKey": "AIzaSyDnwspTgoFuH_5--m7VdV5_ptx7rK1ivlU",
+  "authDomain": "studio-4692583593-18d7f.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "879807515009"
+};
