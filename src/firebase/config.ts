e3a88e8d9bee6c1c3287c488965ecb9f6b80b36b
Murtuza Vohra <murtuza52@gmail.com>
2025-09-25 09:28:58 +0000

    app still not able to connect to firestore. may be reconnect to project

--- src/firebase/config.ts
+++ src/firebase/config.ts
@@ -3,6 +3,7 @@ export const firebaseConfig = {
   "appId": "1:879807515009:web:4111b2591b8c44ad094080",
   "apiKey": "AIzaSyDnwspTgoFuH_5--m7VdV5_ptx7rK1ivlU",
   "authDomain": "studio-4692583593-18d7f.firebaseapp.com",
-  "measurementId": "",
-  "messagingSenderId": "879807515009"
+  "storageBucket": "studio-4692583593-18d7f.appspot.com",
+  "messagingSenderId": "879807515009",
+  "measurementId": "G-5G3J52G54B"
 };
