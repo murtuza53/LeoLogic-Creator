1b60e128abdc9368ec110f8bd29c7194bc92dd46
Murtuza Vohra <murtuza52@gmail.com>
2025-09-27 17:28:07 +0000

    use standard page loader animation

--- src/app/loading.tsx
+++ src/app/loading.tsx
@@ -1,9 +1,9 @@
-import { Logo } from '@/components/icons';
+import { LoaderCircle } from 'lucide-react';
 
 export default function Loading() {
   return (
     <div className="flex min-h-screen items-center justify-center bg-background">
-      <Logo className="h-16 w-16 animate-spin text-primary" />
+      <LoaderCircle className="h-16 w-16 animate-spin text-primary" />
     </div>
   );
 }
