ddc0c2b07b6519065f0f022d70136a6e8942b832
Murtuza Vohra <murtuza52@gmail.com>
2025-09-29 10:46:44 +0000

    in smart product conent
    Image with src "/finaname.png?v=2" is using a q

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export const metadata: Metadata = {
   manifest: '/manifest.json',
   icons: {
     icon: {
-      url: `/favicon.png?v=2`,
+      url: `/favicon.png`,
       type: 'image/png',
     }
   },
@@ -29,10 +29,8 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;600;700&display=swap" rel="stylesheet" />
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4404974079606262" crossOrigin="anonymous"></script>
-             <script async custom-element="amp-auto-ads" src="https://cdn.ampproject.org/v0/amp-auto-ads-0.1.js"></script>
       </head>
       <body className="font-body antialiased">
-      <amp-auto-ads type="adsense" data-ad-client="ca-pub-4404974079606262"></amp-auto-ads>
         <FirebaseClientProvider>
           {children}
           <SpeedInsights />
--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 
 export const Logo = (props: Omit<React.ComponentProps<typeof Image>, 'src' | 'alt'>) => (
   <Image
-    src="/logo.png?v=2"
+    src="/logo.png"
     alt="Leo Creator Logo"
     width={40}
     height={40}
