01ceba807b3f85c8588c321c12015fd7da66c8e8
Murtuza Vohra <murtuza52@gmail.com>
2025-10-08 08:00:26 +0000

    add top-adbanner on each page below title and add bottom-adbanner on eac

--- src/components/top-ad-banner.tsx
+++ src/components/top-ad-banner.tsx
@@ -5,8 +5,8 @@ import Script from 'next/script';
 export default function TopAdBanner() {
   return (
     <div className="container mx-auto px-4 md:px-6 my-8 flex justify-center">
-      <Script async data-cfasync="false" src="//pl27805151.revenuecpmgate.com/94ae52161e71e5c557f12c90150ad810/invoke.js" />
-      <div id="container-94ae52161e71e5c557f12c90150ad810"></div>
+      <Script async data-cfasync="false" src="//pl27805704.revenuecpmgate.com/46b1b74bb423d2f9bdac159949612b31/invoke.js"/>
+      <div id="container-46b1b74bb423d2f9bdac159949612b31"></div>
     </div>
   );
 }
